d831e2ca154c856b358e635e17f30c7847189b0e
tansioooooooo <tansioooooooo@gmail.com>
2019-04-02 10:58:02 +0900

    add before action store_current_location

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :store_current_location, unless: :devise_controller? || :users_controller?
+
+
+  private
+  def store_current_location
+    store_location_for(:user, request.url)
+  end
 
   protected
   def configure_permitted_parameters
